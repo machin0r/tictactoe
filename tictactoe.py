501a877ecdf0405f08437ff7f19bc1d4307f1383
machin0r <machin0r@proton.me>
2023-02-04 13:18:31 +0000

    Simple reinforcement learning method added by adding weighting calculations to AI vs. AI game. AI1 will now pick the 'optimal' choice each turn.

--- tictactoe.py
+++ tictactoe.py
@@ -1,6 +1,8 @@
 import random
 import sys
-sys.setrecursionlimit(1500)
+sys.setrecursionlimit(20000)
+
+numGames = 7500
 
 theBoard = {'1': ' ', '2': ' ', '3': ' ',
             '4': ' ', '5': ' ', '6': ' ',
@@ -9,13 +11,40 @@ solutions = [[1, 2, 3], [4, 5, 6], [7, 8, 9], [1, 4, 7], [2, 5, 8], [3, 6, 9],
              [1, 5, 9], [3, 5, 7]]  # Chars of the same that fill these win
 scoreboard = {}
 
-AI1_placement = {'1': 0, '2': 0, '3': 0,
-                 '4': 0, '5': 0, '6': 0,
-                 '7': 0, '8': 0, '9': 0}
+# This dict is for temporary storage of the posiitons chosen each game
+AI1_placement_temp = {'0': '', '1': '', '2': '',
+                      '3': '', '4': '', '5': '',
+                      '6': '', '7': '', '8': ''}
 
-AI2_placement = {'1': 0, '2': 0,  '3': 0,
-                 '4': 0, '5': 0, '6': 0,
-                 '7': 0, '8': 0, '9': 0}
+# This dictionary stores the weighting of each position choice for each turn
+# Higher numbers indicated that this choice is more likely to win the game
+AI1_full_weighting = {'0': {'1': 0, '2': 0, '3': 0,
+                            '4': 0, '5': 0, '6': 0,
+                            '7': 0, '8': 0, '9': 0},
+                      '1': {'1': 0, '2': 0, '3': 0,
+                            '4': 0, '5': 0, '6': 0,
+                            '7': 0, '8': 0, '9': 0},
+                      '2': {'1': 0, '2': 0, '3': 0,
+                            '4': 0, '5': 0, '6': 0,
+                            '7': 0, '8': 0, '9': 0},
+                      '3': {'1': 0, '2': 0, '3': 0,
+                            '4': 0, '5': 0, '6': 0,
+                            '7': 0, '8': 0, '9': 0},
+                      '4': {'1': 0, '2': 0, '3': 0,
+                            '4': 0, '5': 0, '6': 0,
+                            '7': 0, '8': 0, '9': 0},
+                      '5': {'1': 0, '2': 0, '3': 0,
+                            '4': 0, '5': 0, '6': 0,
+                            '7': 0, '8': 0, '9': 0},
+                      '6': {'1': 0, '2': 0, '3': 0,
+                            '4': 0, '5': 0, '6': 0,
+                            '7': 0, '8': 0, '9': 0},
+                      '7': {'1': 0, '2': 0, '3': 0,
+                            '4': 0, '5': 0, '6': 0,
+                            '7': 0, '8': 0, '9': 0},
+                      '8': {'1': 0, '2': 0, '3': 0,
+                            '4': 0, '5': 0, '6': 0,
+                            '7': 0, '8': 0, '9': 0}}
 
 
 # This function creates the game board
@@ -31,14 +60,17 @@ def printBoard(board):
 def setup():
     for key in sorted(theBoard.keys()):
         theBoard[key] = ' '
+    for key in sorted(AI1_placement_temp.keys()):
+        AI1_placement_temp[key] = ''
 
 
-# Displays winner, adds 1 to score, then asks if the player wants to play again
+# Displays winner, +1 to score, if not AI ask user for another game
+# If it is AI vs. Computer, continues the tournament unti numGames is reached
 def winner(cur_player, player1, player2, game_mode, switch_player, gameNo):
     gameNo = gameNo
     gameNo += 1
     switch_player = not switch_player
-    if game_mode == 0 and gameNo < 500:  # Checks if restarting 1 or 2 player
+    if game_mode == 0 and gameNo < numGames:  # Restarts AI if < numGames
         scoreboard[cur_player] += 1
         AI_players(player1, player2, switch_player, gameNo)
     elif game_mode == 0:
@@ -48,11 +80,9 @@ def winner(cur_player, player1, player2, game_mode, switch_player, gameNo):
         for k, v in scoreboard.items():
             print(k, ':', v)
         print("--------------------------------------------------------------")
-        print(AI1_placement)
+        print(AI1_full_weighting)
         print("-----------------------------------")
-        print(AI2_placement)
-        print("----------------------------d-------")
-    if game_mode == 1 or game_mode == 2:
+    if game_mode == 1 or game_mode == 2:  # Asks the user to play again
         print("Game over! The winner is " + cur_player + "!")
         scoreboard[cur_player] += 1
         print("Current score:")
@@ -67,26 +97,28 @@ def winner(cur_player, player1, player2, game_mode, switch_player, gameNo):
         else:
             exit()
     else:
+        print(AI1_full_weighting)
         exit()
 
 
+# Determines what happens when a game is tied.
+# If over numGames, the tournament is ended
 def tie_game(cur_player, player1, player2, game_mode, switch_player, gameNo):
     gameNo = gameNo
     gameNo += 1
     switch_player = not switch_player
-    if game_mode == 0 and gameNo < 500:  # Check if 1 or 2 player
+    if game_mode == 0 and gameNo < numGames:  # Restarts AI game if < numGames
         AI_players(player1, player2, switch_player, gameNo)
-    else:
+    elif game_mode == 0:
         print("Game Over!")
+        scoreboard[cur_player] += 1
         print("Current score:")
         for k, v in scoreboard.items():
             print(k, ':', v)
         print("--------------------------------------------------------------")
-        print(AI1_placement)
+        print(AI1_full_weighting)
         print("-----------------------------------")
-        print(AI2_placement)
-        print("-----------------------------------")
-    if game_mode == 1 or game_mode == 2:
+    if game_mode == 1 or game_mode == 2:  # Asks the user to play again
         print("It's a tie!")
         print("Current score:")
         for k, v in scoreboard.items():
@@ -100,6 +132,7 @@ def tie_game(cur_player, player1, player2, game_mode, switch_player, gameNo):
         else:
             exit()
     else:
+        print(AI1_full_weighting)
         exit()
 
 
@@ -145,7 +178,6 @@ def two_player(player1, player2, switch_player, gameNo):
     tie_game(cur_player, player1, player2, game_mode, switch_player, gameNo)
 
 
-# This is for 1 player vs the computer
 def single_player(player1, player2, switch_player, gameNo):
     setup()
     game_mode = 1
@@ -220,36 +252,54 @@ def AI_players(player1, player2, switch_player, gameNo):
         players[player1] = 'X'
         players[player2] = 'O'
         cur_player = player1
-    count = 0
 
+    count = 0
     while count < 9:
-        # printBoard(theBoard)
-        # print("-----------------------------------")
-        lastMove = square
-        # Get a random selction where the computer can place
-        square, value = random.choice(list(compBoard.items()))
+        # Gets the computer selection.
+        # If player1, sorts the weighting list for this turn
+        # and picks the highest weighted option.
+        # player2 is picked randomly
+        if cur_player == player1:
+            AI1_test = AI1_full_weighting[str(count)]
+            AI1_placement_weighting_sorted = sorted(AI1_test.items(),
+                                                    key=lambda x: -x[1])
+            # Iterates through the highest weighted positions,
+            # picking the first that is free on the board
+            for pos in AI1_placement_weighting_sorted:
+                if theBoard[pos[0]] == ' ':
+                    square = pos[0]
+                    break
+            AI1_placement_temp[str(count)] = square
+        elif cur_player == player2:
+            square, value = random.choice(list(compBoard.items()))
         theBoard[square] = players[cur_player]
         del compBoard[square]
         count += 1
+
         for sol in solutions:
             if (theBoard[str(sol[0])] == theBoard[str(sol[1])] ==
                     theBoard[str(sol[2])] == players[cur_player]):
+                # If player1 won, +3 to each of the moves played in this game
+                # If player1 lost, -1 to each of the moves played in this game
                 if cur_player == player1:
-                    AI1_placement[square] += 3
-                    AI2_placement[lastMove] -= 1
+                    for key in AI1_placement_temp:
+                        if AI1_placement_temp[key] != '':
+                            AI1_full_weighting[key][AI1_placement_temp[key]] += 3
                 else:
-                    AI1_placement[lastMove] -= 1
-                    AI2_placement[square] += 3
+                    for key in AI1_placement_temp:
+                        if AI1_placement_temp[key] != '':
+                            AI1_full_weighting[key][AI1_placement_temp[key]] -= 1
                 winner(cur_player, player1, player2, game_mode,
                        switch_player, gameNo)
+        # Switch player turn
         if cur_player == player1:
             cur_player = player2
         else:
             cur_player = player1
-    if cur_player == player1:
-        AI1_placement[square] += 1
-    else:
-        AI2_placement[square] += 1
+    # If a draw, +1 to each of the moves played in this game
+    for key in AI1_placement_temp:
+        if AI1_placement_temp[key] != '':
+            AI1_full_weighting[key][AI1_placement_temp[key]] += 1
     tie_game(cur_player, player1, player2, game_mode, switch_player, gameNo)
 
 
